5c97b3c0fbe42e317b9f7b64eb18534d6a54c256
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-09-27 20:49:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Fatima de la Iglesia and is{""}
+        <a
+          href="https://github.com/FatimadelaIglesia/my-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
